7c329400ddc90401053bd8e291234e1329432870
uapasha <uapasha@gmail.com>
2016-07-01 21:07:39 +0300

    fixed parser3

--- parser3.js
+++ parser3.js
@@ -1,28 +1,25 @@
 var res = [{}];
 var num = 0;
 var fs = require('fs');
-var array = fs.readFileSync('sample_movies.txt').toString().split("\n");
-array.forEach((line) => {
+var data = fs.readFileSync('sample_movies.txt').toString().split("\n");
+data.forEach((line) => {
   switch (line.substr(0, 2)) {
     case '':
       num+=1;
       res[num] = {};
       break;
     case 'St':
-      var field = line.split(': ')[0];
-      var stars = line.split(': ')[1]
-      console.log('Stars string ' + stars);
-      
+      var field = line.split(': ')[0].trim();
+      var stars = line.split(': ')[1];
       stars = stars.split(', ');
-      console.log('Stats arraY : ')
-      console.log(stars);
       res[num][field]=[[stars]];
+      break;
     default:
       // code
-      var field = line.split(': ')[0];
+      var field = line.split(': ')[0].trim();
       var value = line.split(': ')[1];
       res[num][field]=value;
   }
-})
+});
 
 console.log(res)
