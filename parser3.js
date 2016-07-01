0b310a174eb9e73a40059a97394606db2ba94edd
Paul <uapasha@gmail.com>
2016-07-01 15:17:55 +0000

    changed parser3

--- parser3.js
+++ parser3.js
@@ -3,12 +3,20 @@ var num = 0;
 var fs = require('fs');
 var array = fs.readFileSync('sample_movies.txt').toString().split("\n");
 array.forEach((line) => {
-  switch (line.substr(0, 1)) {
+  switch (line.substr(0, 2)) {
     case '':
       num+=1;
       res[num] = {};
       break;
-    
+    case 'St':
+      var field = line.split(': ')[0];
+      var stars = line.split(': ')[1]
+      console.log('Stars string ' + stars);
+      
+      stars = stars.split(', ');
+      console.log('Stats arraY : ')
+      console.log(stars);
+      res[num][field]=[[stars]];
     default:
       // code
       var field = line.split(': ')[0];
