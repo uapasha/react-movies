1f17c28c618261adf392b1bbec5f4dd0b0dd12b8
Paul <uapasha@gmail.com>
2016-07-04 14:59:44 +0000

    add file upload form through AJAX

--- client/js/main.jsx
+++ client/js/main.jsx
@@ -267,19 +267,41 @@ class Movie extends React.Component{
 
 ////// File upload form ///////
 class FileUpload extends React.Component{
-render(){
-    return<fieldset>
-        <legend>Upload File</legend>
-        <form id="uploadForm"
-             encType="multipart/form-data"
-             action="https://react-movies-uapasha-c9.c9users.io/api/v1/movies/upload"
-             method="post">
-                <input type="file" name="moviesFile" />
-                <input type="submit" value="Upload File" name="submit"/>
-        </form>
-    </fieldset>
+    
+    uploadFile(e) {
+        e.preventDefault()
+        console.log(ReactDOM.findDOMNode(this.refs.file));
+        var fd = new FormData();    
+        fd.append( 'moviesFile', ReactDOM.findDOMNode(this.refs.file).files[0] );
+    
+        $.ajax({
+            url: 'https://react-movies-uapasha-c9.c9users.io/api/v1/movies/upload',
+            data: fd,
+            processData: false,
+            contentType: false,
+            type: 'POST',
+            success: function(data){
+                alert(data);
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+        
     }
-}
+
+    render(){
+        return<fieldset className="uploader">
+            <legend>Upload File</legend>
+            <form ref="uploadForm"
+                 encType="multipart/form-data"
+                 onSubmit={this.uploadFile.bind(this)}>
+                    <input ref="file" type="file" name="moviesFile" className="upload-file"/>
+                    <input type="submit" value="Upload File" name="submit"/>
+            </form>
+        </fieldset>
+        }
+    }
 
 ////// Add movie form /////////
 
